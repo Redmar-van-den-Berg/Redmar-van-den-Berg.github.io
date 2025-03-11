5cd6040404a4b7e14917de1b32c8f05031728806
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2025-03-11 07:18:37 +0100

    Add argument parser for template file

--- render.py
+++ render.py
@@ -1,11 +1,18 @@
+import os
 import jinja2
 import json
-import sys
+import argparse
 
 if __name__ == "__main__":
-    template_file = "index.html.j2"
+    # Default files
     payload_file = "payload.json"
     external_links_file = "links.json"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--empty', default=False, action='store_true')
+    parser.add_argument('--template', default="index.html.j2")
+
+    args = parser.parse_args()
 
     with open(payload_file) as fin:
         payload = json.load(fin)
@@ -13,13 +20,12 @@ if __name__ == "__main__":
     with open(external_links_file) as fin:
         links = json.load(fin)
 
-    environment = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
-    template = environment.get_template(template_file)
+    template_folder = os.path.dirname(args.template)
+    environment = jinja2.Environment(loader=jinja2.FileSystemLoader(template_folder))
+    template = environment.get_template(os.path.basename(args.template))
 
 
-    if len(sys.argv) == 1:
-        print(template.render(results=payload, links=links, variant="ENST00000375549.8:c.100del"))
-    elif sys.argv[1] == "empty":
+    if args.empty:
         print(template.render())
     else:
-        raise NotImplementedError
+        print(template.render(results=payload, links=links, variant="ENST00000375549.8:c.100del"))
